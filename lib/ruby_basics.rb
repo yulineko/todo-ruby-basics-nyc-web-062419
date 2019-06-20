c910666dbd848af7eada1a4c5a8eb0661b58259b
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-20 16:35:18 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(phrase)
+  return phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting = "Hey there"
+  name = "Bobby"
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(pizza = "cheese")
+  return pizza
 end
