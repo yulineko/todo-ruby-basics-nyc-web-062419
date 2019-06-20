6b03b3f8f2ad1de430caca7b2ac3355e9539e264
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-20 16:25:41 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,17 +1,17 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(phrase)
+  return phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  
 end
 
 def return_a_value
